6b8d0a0a398a9af1e9935894c909cd34586eb26c
YoannDeStena <ydestena@gmail.com>
2025-12-12 16:40:03 +0100

    refactor: Ajout d'une fonction Yatzy#counts pour ne pas à dupliquer du code inutilement

--- src/Yatzy.php
+++ src/Yatzy.php
@@ -115,6 +115,25 @@ class Yatzy
         return $s;
     }
 
+    /**
+     * @param int $d1
+     * @param int $d2
+     * @param int $d3
+     * @param int $d4
+     * @param int $d5
+     * @return array
+     */
+    public static function counts(int $d1, int $d2, int $d3, int $d4, int $d5): array
+    {
+        $counts = array_fill(0, 6, 0);
+        ++$counts[$d1 - 1];
+        ++$counts[$d2 - 1];
+        ++$counts[$d3 - 1];
+        ++$counts[$d4 - 1];
+        ++$counts[$d5 - 1];
+        return $counts;
+    }
+
     public function fours(): int
     {
         $sum = 0;
@@ -161,12 +180,7 @@ class Yatzy
 
     public function scorePair(int $d1, int $d2, int $d3, int $d4, int $d5): int
     {
-        $counts = array_fill(0, 6, 0);
-        ++$counts[$d1 - 1];
-        ++$counts[$d2 - 1];
-        ++$counts[$d3 - 1];
-        ++$counts[$d4 - 1];
-        ++$counts[$d5 - 1];
+        $counts = self::counts($d1, $d2, $d3, $d4, $d5);
         for ($at = 0; $at !== 6; $at++) {
             if ($counts[6 - $at - 1] === 2) {
                 return (6 - $at) * 2;
@@ -177,12 +191,7 @@ class Yatzy
 
     public static function twoPair(int $d1, int $d2, int $d3, int $d4, int $d5): int
     {
-        $counts = array_fill(0, 6, 0);
-        ++$counts[$d1 - 1];
-        ++$counts[$d2 - 1];
-        ++$counts[$d3 - 1];
-        ++$counts[$d4 - 1];
-        ++$counts[$d5 - 1];
+        $counts = self::counts($d1, $d2, $d3, $d4, $d5);
         $n = 0;
         $score = 0;
         for ($i = 0; $i !== 6; $i++) {
@@ -201,12 +210,7 @@ class Yatzy
 
     public static function threeOfAKind(int $d1, int $d2, int $d3, int $d4, int $d5): int
     {
-        $t = array_fill(0, 6, 0);
-        ++$t[$d1 - 1];
-        ++$t[$d2 - 1];
-        ++$t[$d3 - 1];
-        ++$t[$d4 - 1];
-        ++$t[$d5 - 1];
+        $t = self::counts($d1, $d2, $d3, $d4, $d5);
         for ($i = 0; $i !== 6; $i++) {
             if ($t[$i] >= 3) {
                 return ($i + 1) * 3;
@@ -217,12 +221,7 @@ class Yatzy
 
     public static function smallStraight(int $d1, int $d2, int $d3, int $d4, int $d5): int
     {
-        $tallies = array_fill(0, 6, 0);
-        ++$tallies[$d1 - 1];
-        ++$tallies[$d2 - 1];
-        ++$tallies[$d3 - 1];
-        ++$tallies[$d4 - 1];
-        ++$tallies[$d5 - 1];
+        $tallies = self::counts($d1, $d2, $d3, $d4, $d5);
         if ($tallies[0] === 1 &&
             $tallies[1] === 1 &&
             $tallies[2] === 1 &&
@@ -235,12 +234,7 @@ class Yatzy
 
     public static function largeStraight(int $d1, int $d2, int $d3, int $d4, int $d5): int
     {
-        $tallies = array_fill(0, 6, 0);
-        ++$tallies[$d1 - 1];
-        ++$tallies[$d2 - 1];
-        ++$tallies[$d3 - 1];
-        ++$tallies[$d4 - 1];
-        ++$tallies[$d5 - 1];
+        $tallies = self::counts($d1, $d2, $d3, $d4, $d5);
         if ($tallies[1] === 1 &&
             $tallies[2] === 1 &&
             $tallies[3] === 1 &&
@@ -258,12 +252,7 @@ class Yatzy
         $_3 = false;
         $_3_at = 0;
 
-        $tallies = array_fill(0, 6, 0);
-        ++$tallies[$d1 - 1];
-        ++$tallies[$d2 - 1];
-        ++$tallies[$d3 - 1];
-        ++$tallies[$d4 - 1];
-        ++$tallies[$d5 - 1];
+        $tallies = self::counts($d1, $d2, $d3, $d4, $d5);
 
         foreach (range(0, 5) as $i) {
             if ($tallies[$i] === 2) {
